9609932c29632edf8c0fd74d03f61c12b4adba74
Marius <mariusae@me.com>
2022-04-27 11:42:57 +0300

    Initial Commit

---
+++ Objective-C Fundamentals/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C Fundamentals
+//
+//  Created by Marius Malyshev on 27.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
