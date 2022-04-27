13be740f1d8897983cec2bc3f4f3ab9cbb5bc7a0
Marius <mariusae@me.com>
2022-04-27 12:04:49 +0300

    RSCar

---
+++ Objective-C Fundamentals/Model/Car/RSCar.h
@@ -0,0 +1,18 @@
+//
+//  RSCar.h
+//  Objective-C Fundamentals
+//
+//  Created by Marius Malyshev on 27.04.2022.
+//
+
+#import "RSVehicle.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RSCar : RSVehicle
+
+- (void)move;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Objective-C Fundamentals/Model/Car/RSCar.m
@@ -0,0 +1,16 @@
+//
+//  RSCar.m
+//  Objective-C Fundamentals
+//
+//  Created by Marius Malyshev on 27.04.2022.
+//
+
+#import "RSCar.h"
+
+@implementation RSCar
+
+-(void)move {
+    NSLog(@"I'm driving, the driver is %@", self.captain.name);
+}
+
+@end
