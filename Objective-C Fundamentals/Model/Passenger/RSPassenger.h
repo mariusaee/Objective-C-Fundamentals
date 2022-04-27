076c856d00f1a36c59842d206ba58745912d9cfe
Marius <mariusae@me.com>
2022-04-27 11:45:26 +0300

    RSPassenger template

---
+++ Objective-C Fundamentals/Model/Passenger/RSPassenger.h
@@ -0,0 +1,16 @@
+//
+//  RSPassenger.h
+//  Objective-C Fundamentals
+//
+//  Created by Marius Malyshev on 27.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RSPassenger : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Objective-C Fundamentals/Model/Passenger/RSPassenger.m
@@ -0,0 +1,12 @@
+//
+//  RSPassenger.m
+//  Objective-C Fundamentals
+//
+//  Created by Marius Malyshev on 27.04.2022.
+//
+
+#import "RSPassenger.h"
+
+@implementation RSPassenger
+
+@end
